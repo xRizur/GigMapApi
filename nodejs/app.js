d89a394c1b8bf714a57f3b2cf210850cbdc7a923
Maciej Pająk <xRizur@o2.pl>
2025-06-14 21:04:25 +0000

    final v1 on server

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bandsRouter = require('./routes/bands');
 const pubsRouter = require('./routes/pubs');
 const requestsRouter = require('./routes/requests');
@@ -8,14 +9,32 @@ const connectDB = require('./config/db');
 
 connectDB();
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-app.use('/bands',bandsRouter);
+app.use('/bands', bandsRouter);
 app.use('/pubs', pubsRouter);
 app.use('/uploads', express.static('uploads'));
 app.use('/requests', requestsRouter);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/bands/new', (req, res) => {
+  res.render('band_form');
+});
+
+app.get('/pubs/new', (req, res) => {
+  res.render('pub_form');
+});
+
+app.get('/requests/new', (req, res) => {
+  res.render('request_form');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
