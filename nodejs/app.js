9024f9d1e2d2e79b055b56c42be54b17341b7a9a
Maciej Pająk <xRizur@o2.pl>
2025-06-15 11:03:29 +0000

    reverted changes

--- nodejs/app.js
+++ nodejs/app.js
@@ -8,29 +8,41 @@ require('dotenv').config();
 const connectDB = require('./config/db');
 
 connectDB();
+
+// Pobierz BASE_PATH z zmiennych środowiskowych (np. /p26)
+const BASE_PATH = process.env.USER || process.env.PORT;
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/bands', bandsRouter);
-app.use('/pubs', pubsRouter);
-app.use('/uploads', express.static('uploads'));
-app.use('/requests', requestsRouter);
+// Helper do generowania URL-i dostępny w wszystkich templateach
+app.locals.basePath = BASE_PATH;
+app.locals.url = (path) => BASE_PATH + path;
 
-app.get('/', (req, res) => {
+// Statyczne pliki
+app.use(BASE_PATH + '/uploads', express.static('uploads'));
+
+// Routery z prefixem
+app.use(BASE_PATH + '/bands', bandsRouter);
+app.use(BASE_PATH + '/pubs', pubsRouter);
+app.use(BASE_PATH + '/requests', requestsRouter);
+
+// Główne endpointy
+app.get(BASE_PATH + '/', (req, res) => {
   res.render('home');
 });
 
-app.get('/bands/new', (req, res) => {
+app.get(BASE_PATH + '/bands/new', (req, res) => {
   res.render('band_form');
 });
 
-app.get('/pubs/new', (req, res) => {
+app.get(BASE_PATH + '/pubs/new', (req, res) => {
   res.render('pub_form');
 });
 
-app.get('/requests/new', (req, res) => {
+app.get(BASE_PATH + '/requests/new', (req, res) => {
   res.render('request_form');
 });
 
