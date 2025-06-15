41e5cb26c21531d2da09643361b887ec908abac7
Maciej Pająk <xRizur@o2.pl>
2025-06-15 14:21:08 +0000

    delete comment

--- nodejs/app.js
+++ nodejs/app.js
@@ -9,11 +9,7 @@ const connectDB = require('./config/db');
 
 connectDB();
 
-// Użyj zmiennej środowiskowej APP_BASE_PATH do określenia publicznego prefiksu ścieżki.
-// Na serwerze produkcyjnym ustaw APP_BASE_PATH na np. /p26.
-// Lokalnie, jeśli nie jest ustawiona, będzie to pusty string.
-// Zmieniamy źródło publicBasePath na process.env.PORT
-let publicBasePath = process.env.PORT || ''; // ZMIANA TUTAJ: Użyj PORT zamiast APP_BASE_PATH
+let publicBasePath = process.env.PORT || ''; 
 if (publicBasePath && !publicBasePath.startsWith('/')) {
   publicBasePath = '/' + publicBasePath;
 }
@@ -25,7 +21,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use((req, res, next) => {
-  // Udostępnij publicBasePath do szablonów EJS jako 'basePath'
   res.locals.basePath = publicBasePath;
   next();
 });
@@ -33,19 +28,15 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Trasy są teraz definiowane bez publicBasePath.
-// Serwer proxy powinien mapować np. /p26/bands na /bands w aplikacji.
 app.use('/bands', bandsRouter);
 app.use('/pubs', pubsRouter);
-app.use('/uploads', express.static('uploads')); // Ścieżka do zasobów statycznych
+app.use('/uploads', express.static('uploads')); 
 app.use('/requests', requestsRouter);
 
-// Główna strona aplikacji
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-// Trasy do formularzy (jeśli nie są obsługiwane wewnątrz dedykowanych routerów)
 app.get('/bands/new', (req, res) => {
   res.render('band_form');
 });
@@ -54,9 +45,6 @@ app.get('/pubs/new', (req, res) => {
   res.render('pub_form');
 });
 
-// Zakładamy, że /requests/new jest obsługiwane przez requestsRouter,
-// więc nie ma potrzeby definiowania app.get('/requests/new', ...) tutaj,
-// chyba że jest inaczej.
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
