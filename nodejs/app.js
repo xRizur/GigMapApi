6b3078149204b58bf35e0687e7c82b5f3a663318
Maciej Pająk <xRizur@o2.pl>
2025-06-15 10:48:30 +0000

    d

--- nodejs/app.js
+++ nodejs/app.js
@@ -9,56 +9,64 @@ const connectDB = require('./config/db');
 
 connectDB();
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); // Ścieżka do szablonów EJS
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const mainRouter = express.Router();
-const host = process.env.USER;
+const host = process.env.USER; // Np. 'p26'
+const actualBasePath = host ? `/${host}` : ''; // Wynik: '/p26' lub ''
 
-mainRouter.use((req, res, next) => {
-  res.locals.baseUrl = req.baseUrl; // req.baseUrl to ścieżka, pod którą mainRouter jest zamontowany
+// Udostępnij 'baseUrl' wszystkim szablonom EJS.
+// Ta zmienna będzie zawierać prefiks aplikacji (np. '/p26').
+app.use((req, res, next) => {
+  res.locals.baseUrl = actualBasePath;
   next();
 });
 
+// Serwowanie ogólnych zasobów statycznych (np. CSS, JS) z katalogu 'public'
+// Zakładamy, że katalog 'public' jest na tym samym poziomie co 'nodejs', czyli w /workspaces/GigMapApi/public
+// Te zasoby będą dostępne pod adresem: <%= baseUrl %>/nazwa_pliku.css (np. /p26/style.css)
+const publicDirectoryPath = path.join(__dirname, '../public');
+app.use(actualBasePath, express.static(publicDirectoryPath));
 
-mainRouter.use(express.static(path.join(__dirname, 'public')));
-
+// Definicje tras w głównym routerze
 mainRouter.use('/bands', bandsRouter);
 mainRouter.use('/pubs', pubsRouter);
+// Serwowanie zasobów z katalogu 'uploads' (np. obrazki)
+// Dostępne pod adresem: <%= baseUrl %>/uploads/nazwa_obrazka.jpg (np. /p26/uploads/obrazek.jpg)
 mainRouter.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 mainRouter.use('/requests', requestsRouter);
 
 mainRouter.get('/', (req, res) => {
-  res.render('home'); // res.locals.baseUrl będzie dostępne
+  // res.locals.baseUrl jest już dostępne w szablonie
+  res.render('home');
 });
 
 mainRouter.get('/bands/new', (req, res) => {
-  res.render('band_form'); // res.locals.baseUrl będzie dostępne
+  res.render('band_form');
 });
 
 mainRouter.get('/pubs/new', (req, res) => {
-  res.render('pub_form'); // res.locals.baseUrl będzie dostępne
+  res.render('pub_form');
 });
 
 mainRouter.get('/requests/new', (req, res) => {
-  res.render('request_form'); // res.locals.baseUrl będzie dostępne
+  res.render('request_form');
 });
 
 const port = process.env.PORT || 3000;
 
-// Montujemy mainRouter pod ścieżką główną
-app.use('/', mainRouter);
-
-// Jeśli host (np. p26) jest zdefiniowany, montujemy mainRouter również pod tym prefiksem
-if (host) {
-  app.use(`/${host}`, mainRouter);
-}
+// Zamontuj główny router aplikacji pod ustalonym 'actualBasePath'
+// Jeśli 'actualBasePath' to '/p26', wszystkie trasy z mainRouter będą dostępne pod /p26/nazwa_trasy
+// Jeśli 'actualBasePath' jest puste, trasy będą dostępne pod /nazwa_trasy
+app.use(actualBasePath, mainRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    console.log(`App available at http://localhost:${port}`);
-    if (host) {
-        console.log(`App also available at http://localhost:${port}/${host}`);
+    console.log(`Serwer działa na porcie ${port}`);
+    if (actualBasePath) {
+        console.log(`Aplikacja dostępna pod adresem: http://localhost:${port}${actualBasePath}`);
+    } else {
+        console.log(`Aplikacja dostępna pod adresem: http://localhost:${port}`);
     }
 });
